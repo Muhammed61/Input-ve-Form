81633a66fa090ca75489f29d3399d732788cd3db
Muhammed Günaydın <43293093+Muhammed61@users.noreply.github.com>
2021-08-26 13:38:15 +0300

    Input ve Form veri gönderme

---
+++ src/App.vue
@@ -0,0 +1,148 @@
+<template>
+<div class="container">
+    <h3>Form Verileriyle Çalışmak</h3>
+    <hr>
+    <div class="row">
+        <div class="col-md-6">
+            <div class="panel panel-warning">
+                <div class="panel-heading">
+                    <h4>Başvuru Formu</h4>
+                </div>
+                <div class="panel-body">
+                    <form>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <label for="email">Kullanıcı Adı</label>
+                                    <input type="text" id="username" class="form-control" v-model="userData.username">
+                                </div>
+                                <div class="form-group">
+                                    <label for="password">Şifre</label>
+                                    <input type="password" id="password" class="form-control" v-model.lazy="userData.password">
+                                </div>
+                                <div class="form-group">
+                                    <label for="age">Yaş</label>
+                                    <input type="number" id="age" class="form-control" v-model="userData.age">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12 form-group">
+                                <label for="message">Açıklama</label><br>
+                                <textarea v-model="userData.message" id="message" rows="3" class="form-control"></textarea>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <label>
+                                        <input v-model="userData.interests" type="checkbox" value="yazilim"> Yazılım
+                                    </label>
+                                    <label>
+                                        <input v-model="userData.interests" type="checkbox" value="donanim"> Donanım
+                                    </label>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12 form-group">
+                                <label>
+                                    <input v-model="userData.gender" type="radio" value="Erkek"> Erkek
+                                </label>
+                                <label>
+                                    <input v-model="userData.gender" type="radio" value="Kadın"> Kadın
+                                </label>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12 from-group">
+                                <label>Şehir</label>
+                                <select v-model="userData.selectedCity" class="form-control">
+                                    <option :selected="city == 'Adana'" v-for="city in userData.cities" :key="city.id">{{ city }}</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12 form-group">
+                                <app-switch v-model="switched"></app-switch>
+                            </div>
+                        </div>
+                        <hr>
+
+                        <div class="row">
+                            <div class="col-md-12">
+                                <button @click.prevent="submit" class="btn btn-primary">Gönder!
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6" v-if="isSubmitted">
+            <div class="panel panel-info">
+                <div class="panel-heading">
+                    <h4>Form Verileri</h4>
+                </div>
+                <div class="panel-body">
+                    <p>Kullanıcı Adı: {{ userData.username }}</p>
+                    <p>Şifre: {{ userData.password }}</p>
+                    <p>Yaş: {{ userData.age }}</p>
+                    <p style="white-space: pre;">Açıklama: {{ userData.message }}</p>
+                    <p><strong>İlgi Alanları</strong></p>
+                    <ul>
+                        <li v-for="item in userData.interests" :key="item.id"> {{ item }}</li>
+                    </ul>
+                    <p>Cinsiyet: {{ userData.gender }}</p>
+                    <p>Şehir: {{ userData.selectedCity }}</p>
+                    <p>Toggle: {{ switched }}</p>
+                </div>
+            </div>
+        </div>
+        <app-form></app-form>
+
+    </div>
+
+</div>
+</template>
+
+<script>
+import Switch from './Switch';
+import Form from './Form';
+
+
+export default {
+    components: {
+        appSwitch: Switch,
+        appForm: Form
+    },
+    data() {
+        return {
+            userData: {
+                username: '',
+                password: '',
+                age: null,
+                message: '',
+                interests: [],
+                gender: '',
+                cities: ['Adana', 'Ankara', 'İstanbul', 'İzmir', 'Bursa', 'Trabzon'],
+                selectedCity: ''
+            },
+            switched: true,
+            isSubmitted : false
+
+        }
+
+    },
+    methods: {
+        submit() {
+          this.isSubmitted = true
+        }
+    },
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/Form.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="container">
+    <form v-if="!isSubmitted">
+      <div class="row" >
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+         
+          <app-fullname v-model="fullname"></app-fullname>
+
+          <div class="form-group">
+            <label>E-mail bilgisi</label>
+            <input type="text" class="form-control" v-model="email">
+          </div>
+          <div class="form-group">
+            <label>Şifre bilgisi</label>
+            <input type="password" class="form-control" v-model="password">
+          </div>
+          <div class="form-group">
+            <label>
+              <input type="radio" v-model="gender" value="kadin"> Kadın
+            </label>
+            <label>
+              <input type="radio" v-model="gender" value="erkek"> Erkek
+            </label>
+          </div>
+          <button @click.prevent="submit" class="btn btn-primary">Kayıt Ol</button>
+          
+        </div>
+      </div>
+    </form>
+    <div v-else class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <div class="panel panel-info">
+          <div class="panel-heading">
+            <h4>Girdiğiniz Bilgier</h4>
+          </div>
+          <div class="panel-body">
+            <p>Tam Ad {{ fullname }}</p>
+            <p>E-mail {{ email }}</p>
+            <p>Şifre {{ password}}</p>
+            <p>Cinsiyet {{ gender }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Fullname from "./Fullname";
+
+  export default {
+    components : {
+      appFullname : Fullname
+    },
+    data(){
+      return {
+        // firstName : '',
+        // lastName : '',
+        fullname : 'Muhammed Günaydın',
+        email : '',
+        password : '',
+        gender : '',
+        isSubmitted : false
+      }
+    },
+    methods : {
+      submit(){
+        this.isSubmitted = true;
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
